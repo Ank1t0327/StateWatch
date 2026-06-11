d5142f0eeb0a7d6a45c3aad1b67333c4ce9f7cfe
Ank1t0327 <ank1t032718@gmail.com>
2026-06-11 15:08:19 +0530

    cpu monitor added

--- modules/cpu_monitor.sh
+++ modules/cpu_monitor.sh
@@ -7,15 +7,30 @@ echo "          CPU MONITOR"
 echo "================================"
 echo 
 
+
+current_user=$(whoami)
+hostname=$(hostname)
+kernal_version=$(uname -r)
+
 cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2}')
 
 load_avg=$(uptime | awk -F'load average:' '{print $2}')
 
-cores=$(nproc)
+cores=$(nproc)                               #. nproc is a command that returns the number of processing units available, which is typically the number of CPU cores.
+                                             #. This is called Command Substitution. ex. date_now=$(date)
 
-echo "CPU Usage     : $cpu_usage %"
-echo "Load Average  : $load_avg"
-echo "CPU CORES     : $cores"
+
+echo "Current User       : $current_user"
+echo "Hostname           : $hostname"
+echo "Kernal Version     : $kernal_version"
+
+echo "CPU Usage          : $cpu_usage %"
+echo "Load Average       : $load_avg"
+echo "CPU CORES          : $cores"
+
+top_process=$(ps -eo comm,%cpu --sort=-%cpu | grep -v "^ps" | sed -n '2p' | awk '{print $1}')
+echo
+echo "Top CPU Process    : $top_process"
 
 echo 
 read -p "Press Enter to return..."
